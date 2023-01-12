3759364e604cd2abd25948599c4dfe9d5db70a92
Damithkumara <damithkumararoxx55@gmail.com>
2023-01-12 22:21:16 +0530

    complete news latter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Navbar , Hero , Memory, Explore , Footer, Advertise, Pricing, Banner } from './components';
-import { hero , navlinks , memory , placesAPI , brands , pricingapi , bannerAPI} from './data/travigodata';
+import { Navbar , Hero , Memory, Explore , Footer, Advertise, Pricing, Banner, Newslatter } from './components';
+import { hero , navlinks , memory , placesAPI , brands , pricingapi , bannerAPI , footerAPI} from './data/travigodata';
 
 const App = () => {
   return (
@@ -12,7 +12,8 @@ const App = () => {
       <Advertise brands={brands}/>
       <Pricing pricingapi={pricingapi}/>
       <Banner bannerAPI={bannerAPI}/>
-      <Footer/>
+      <Newslatter />
+      <Footer footerAPI={footerAPI}/>
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,5 @@ export {default as Explore} from './Explore.jsx';
 export {default as Advertise} from './Advertise.jsx';
 export {default as Pricing} from './Pricing.jsx';
 export {default as Banner} from './Banner.jsx';
+export {default as Newslatter} from './Newslatter.jsx';
 export {default as Footer} from './Footer.jsx';
